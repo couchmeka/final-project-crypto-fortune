2ca3777d4c3420d15b6604aa64900816411310cd
CouchMeka <meka.daniels1@gmail.com>
2022-12-06 10:57:49 -0500

    updating table sort

--- main.js
+++ main.js
@@ -251,16 +251,17 @@ cookieButton.addEventListener('click', () => {
 
 
 
-var getCellValue = function(tr, idx){ return tr.children[idx].innerText || tr.children[idx].textContent; }
+let getCellValue = function(tr, idx){ return tr.children[idx].innerText || tr.children[idx].textContent; }
 
-var comparer = function(idx, asc) { return function(a, b) { return function(v1, v2) {
+// (idx = columnIndex, asc = ascending order)
+let comparer = function(idx, asc) { return function(a, b) { return function(v1, v2) {
         return v1 !== '' && v2 !== '' && !isNaN(v1) && !isNaN(v2) ? v1 - v2 : v1.toString().localeCompare(v2);
     }(getCellValue(asc ? a : b, idx), getCellValue(asc ? b : a, idx));
 }};
 
 // do the work...
 Array.prototype.slice.call(document.querySelectorAll('th')).forEach(function(th) { th.addEventListener('click', function() {
-        var table = th.parentNode
+        let table = th.parentNode
         while(table.tagName.toUpperCase() != 'TABLE') table = table.parentNode;
         Array.prototype.slice.call(table.querySelectorAll('tr:nth-child(n+2)'))
             .sort(comparer(Array.prototype.slice.call(th.parentNode.children).indexOf(th), this.asc = !this.asc))
